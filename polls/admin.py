cf253566dd2e4cb42f6966d201d4c3f0ff94b344
id2k1149 <id2k1149@gmail.com>
2021-07-16 11:31:39 -0400

    add manytomany to admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,24 +2,24 @@ from django.contrib import admin
 from .models import Question, Answer, Description, Record
 
 
-# class AnswerInline(admin.TabularInline):
-#     model = Answer
-#     extra = 2
-#
-#
-# class QuestionAdmin(admin.ModelAdmin):
-#     fieldsets = [
-#         (None,
-#          {'fields': ['title', 'is_active']}
-#          ),
-#         ('Date info',
-#          {'fields': ['date_published'],
-#           'classes': ['collapse']}
-#          ),
-#     ]
-#     inlines = [AnswerInline]
-#
-#     list_display = ('title', 'date_published', 'is_active')
+class AnswerInline(admin.TabularInline):
+    model = Question.answers.through
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,
+         {'fields': ['title', 'is_active']}
+         ),
+        ('Date info',
+         {'fields': ['date_published'],
+          'classes': ['collapse']}
+         ),
+    ]
+    inlines = [AnswerInline]
+
+    list_display = ('title', 'date_published', 'is_active')
 
 
 class RecordAdmin(admin.ModelAdmin):
@@ -27,8 +27,8 @@ class RecordAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
-# admin.site.register(Question, QuestionAdmin)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Description)
 admin.site.register(Record, RecordAdmin)
